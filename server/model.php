548603963a2ff1b9bc4f8e228a21130cade813d5
Ngom Magueye <ngom14@mmi-dev.unilim.fr>
2026-04-22 13:41:32 +0200

    update credentials

--- server/model.php
+++ server/model.php
@@ -14,9 +14,9 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "morap01");
-define("DBLOGIN", "morap01");
-define("DBPWD", "morap01");
+define("DBNAME", "ngom14");
+define("DBLOGIN", "ngom14");
+define("DBPWD", "ngom14");
 
 
 function getAllMovies(){
